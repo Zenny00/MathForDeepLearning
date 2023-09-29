ada2a9ee34e25cb9f118e839ca1658a5439b8167
Zenny <joshuacomfort@gmail.com>
2023-09-29 08:55:15 -0400

    Add learn_numpy.py

---
+++ ChapterOne/learn_numpy.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+print(np.__version__)
+
+a = np.array([1,2,3,4])
+
+print("Array: {}".format(a))
+print("Size: {}".format(a.size))
+print("Shape: {}".format(a.shape))
+print("Data Type: {}".format(a.dtype))
