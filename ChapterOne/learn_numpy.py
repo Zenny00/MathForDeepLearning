0da2bc196bfafd463729018d44deb884b707e340
Zenny <joshuacomfort@gmail.com>
2023-09-29 21:44:44 -0400

    Add 2D array

--- ChapterOne/learn_numpy.py
+++ ChapterOne/learn_numpy.py
@@ -8,3 +8,8 @@ print("Array: {}".format(a))
 print("Size: {}".format(a.size))
 print("Shape: {}".format(a.shape))
 print("Data Type: {}".format(a.dtype))
+
+# Array of arrays
+b = np.array([[1,2,3,4],[5,6,7,8]])
+print(b)
+print(b.shape)
